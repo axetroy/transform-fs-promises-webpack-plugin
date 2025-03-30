822c779597cc621c15181d1441193f3b03bef36b
Axetroy <axetroy.dev@gmail.com>
2025-03-30 14:46:20 +0800

    更新 babel-plugin-transform-fs-promises 到 1.0.2，添加 babel-transform-webpack-plugin 依赖，并调整相关代码以支持新插件

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import BabelPluginTransformFsPromises from "babel-plugin-transform-fs-promises";
-import { transformAsync } from "@babel/core";
+import { babelPluginTransformFsPromises } from "babel-plugin-transform-fs-promises";
+import BabelTransformPlugin from "babel-transform-webpack-plugin";
 import type { TransformOptions } from "@babel/core";
-import type { Compiler, Compilation } from "webpack";
+import type { Compiler } from "webpack";
 
 /**
  * 将 fs.promises 替换为 fs 以兼容低版本 Node.js
@@ -17,100 +17,14 @@ class TransformFsPromisesPlugin {
     }
 
     apply(compiler: Compiler) {
-        const { webpack } = compiler;
-        this.isWebpack5 = webpack && webpack.version.startsWith("5");
-
-        if (this.isWebpack5) {
-            // Webpack 5 逻辑
-            this.applyWebpack5(compiler);
-        } else {
-            // Webpack 4 逻辑
-            this.applyWebpack4(compiler);
-        }
-    }
-
-    /**
-     * Webpack 5 实现（使用 processAssets）
-     */
-    private applyWebpack5(compiler: Compiler) {
-        compiler.hooks.thisCompilation.tap("TransformFsPromisesPlugin", (compilation: Compilation) => {
-            compilation.hooks.processAssets.tapPromise(
-                {
-                    name: "TransformFsPromisesPlugin",
-                    stage: compiler.webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE,
-                },
-                async (assets) => {
-                    await this.processAllAssets(compiler, compilation, assets);
-                },
-            );
-        });
-    }
-
-    /**
-     * Webpack 4 实现（使用 optimize-chunk-assets）
-     */
-    private applyWebpack4(compiler: Compiler) {
-        compiler.hooks.compilation.tap("TransformFsPromisesPlugin", (compilation: Compilation) => {
-            // Webpack 4 没有 processAssets，使用 optimizeChunkAssets
-            compilation.hooks.optimizeChunkAssets.tapPromise("TransformFsPromisesPlugin", async (chunks) => {
-                const assets: Record<string, any> = {};
-                chunks.forEach((chunk) => {
-                    chunk.files.forEach((file) => {
-                        assets[file] = compilation.assets[file];
-                    });
-                });
-                await this.processAllAssets(compiler, compilation, assets);
-            });
-        });
-    }
-
-    /**
-     * 处理所有资源（公共方法）
-     */
-    private async processAllAssets(compiler: Compiler, compilation: Compilation, assets: Record<string, any>): Promise<void> {
-        const javascriptFiles = Object.keys(assets).filter((filename) => filename.endsWith(".js"));
-
-        const tasks = javascriptFiles.map(async (filename) => {
-            const originalSource = assets[filename].source();
-            const sourceStr = typeof originalSource === "string" ? originalSource : originalSource.toString();
-
-            const modifiedSource = await this.replaceContent(sourceStr, filename);
-
-            if (modifiedSource === sourceStr) {
-                return;
-            }
-
-            // Webpack 4/5 通用的更新资源方式
-            if (this.isWebpack5) {
-                // 更新资源
-                compilation.updateAsset(filename, new compiler.webpack.sources.RawSource(modifiedSource));
-            } else {
-                // @ts-expect-error
-                compilation.assets[filename] = {
-                    source: () => modifiedSource,
-                    size: () => modifiedSource.length,
-                };
-            }
-        });
-
-        await Promise.all(tasks);
-    }
-
-    /**
-     * 使用 Babel 转换代码
-     */
-    private async replaceContent(source: string, filename: string): Promise<string> {
-        const result = await transformAsync(source, {
+        new BabelTransformPlugin({
             ...this.transformOptions,
-            filename,
-            plugins: [...(this.transformOptions.plugins ?? []), BabelPluginTransformFsPromises].filter(Boolean),
-        });
-
-        if (!result?.code) {
-            throw new Error("Babel transform failed");
-        }
-
-        return result.code;
+            transformOptions: {
+                plugins: ([babelPluginTransformFsPromises] as TransformOptions["plugins"])
+                    .concat(this.transformOptions.plugins ?? [])
+                    .filter(Boolean),
+            },
+        }).apply(compiler);
     }
 }
 
